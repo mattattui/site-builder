1c8025f72a734af3364bc93069c4b5e8353ffe8e
Matt Robinson <matt@inanimatt.com>
2012-04-22 03:52:29 +0100

    Doc-comments

--- src/Inanimatt/SiteBuilder/SiteBuilder.php
+++ src/Inanimatt/SiteBuilder/SiteBuilder.php
@@ -26,6 +26,12 @@ class SiteBuilder
     }
     
     
+    /**
+     * Register a renderer to handle the given file extension(s)
+     * 
+     * @param RendererInterface $renderer An object that implements the RendererInterface
+     * @param array $extensions One or more file extensions without the initial .
+     */
     public function registerRenderer(RendererInterface $renderer, $extensions) {
         if (!is_array($extensions)) {
             throw new SiteBuilderException('Invalid argument: 2nd argument must be an array.');
@@ -36,18 +42,30 @@ class SiteBuilder
         }
     }
     
+    /**
+     * Set default template filename
+     * 
+     * @param string $template Template filename (without path)
+     */
     public function setDefaultTemplate($template)
     {
         $this->default_template = $template;
     }
 
+    /**
+     * Set template search path
+     * 
+     * @param string $path Path to templates
+     */
     public function setTemplatePath($path)
     {
         $this->template_path = $path;
     }
         
     
-    // Get a list of content objects, run the renderer on each one, then serialise them
+    /**
+     * Iterate through the content collection, render each file, serialise output
+     */
     public function renderSite()
     {
         $contentObjects = $this->contentCollection->getObjects();
@@ -57,7 +75,12 @@ class SiteBuilder
         }
     }
     
-    
+    /**
+     * Render given content object with chosen (or default) template, and return output
+     * 
+     * @param ContentHandlerInterface $file ContentHandler object
+     * @return string Rendered content
+     */
     public function renderFile(ContentHandlerInterface $file)
     {
         // Handle content and data first
@@ -69,7 +92,8 @@ class SiteBuilder
         }
         
         // Insert content into template
-        // FIXME: templates can only ever be files while splFileInfo used to determine type
+        // FIXME: templates can only ever be files while splFileInfo used to determine type. 
+        // Create template handler object, like content handler? 
         $templateInfo = new \splFileInfo($data['template']);
         
         if (!isset($this->renderers[$templateInfo->getExtension()])) {
