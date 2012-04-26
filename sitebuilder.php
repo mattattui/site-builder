fda0f67cbc3cce2f3dbf291e6eaab356f4ccb928
Matt Robinson <matt@inanimatt.com>
2012-04-26 19:42:19 +0100

    Fix version number

--- sitebuilder.php
+++ sitebuilder.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-$console = new Application('SiteBuilder', '1.0-dev');
+$console = new Application('SiteBuilder', '2.0-dev');
 
 $console
     ->register('rebuild')
