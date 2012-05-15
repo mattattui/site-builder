e62c0b834186c7c4e652539e082d8050facd7b2d
Matt Robinson <matt@tui.co.uk>
2012-05-15 18:12:10 +0100

    Generated API docs

--- src/Inanimatt/SiteBuilder/Renderer/TwigRenderer.php
+++ src/Inanimatt/SiteBuilder/Renderer/TwigRenderer.php
@@ -5,11 +5,19 @@ class TwigRenderer implements RendererInterface
 {
     protected $twig;
 
-    public function __construct($twig)
+    /**
+     * Create a new renderer
+     * 
+     * @param Twig_Loader $twig A Twig_Loader instance
+     */
+    public function __construct(\Twig_Loader $twig)
     {
         $this->twig = $twig;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function render($data, $template)
     {
         return $this->twig->render($template, $data);
