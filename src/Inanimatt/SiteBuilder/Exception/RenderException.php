629de4cdda46f6f11788e9b1d8b098aec8b312a6
Matt Robinson <matt@inanimatt.com>
2012-05-19 17:56:09 +0200

    Mark up exception message

--- src/Inanimatt/SiteBuilder/Exception/RenderException.php
+++ src/Inanimatt/SiteBuilder/Exception/RenderException.php
@@ -3,4 +3,11 @@ namespace Inanimatt\SiteBuilder\Exception;
 
 class RenderException extends SiteBuilderException 
 {
+    
+    public function __construct($message = null, $code = 0, Exception $previous = null)
+    {
+        $message = sprintf('Renderer exception: %s', $message);
+        return parent::__construct($message, $code, $previous);
+    }
+    
 }
