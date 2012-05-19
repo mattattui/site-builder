84d2273c5096cb75f04ff57c2d7473268764124a
Matt Robinson <matt@inanimatt.com>
2012-05-19 17:55:55 +0200

    Remove pointless doc comment

--- src/Inanimatt/SiteBuilder/Exception/ArgumentException.php
+++ src/Inanimatt/SiteBuilder/Exception/ArgumentException.php
@@ -4,10 +4,6 @@ namespace Inanimatt\SiteBuilder\Exception;
 class ArgumentException extends SiteBuilderException 
 {
 
-
-    /**
-     * @inheritDoc
-     */
     public function __construct($message = null, $code = 0, Exception $previous = null)
     {
         $message = sprintf('Argument exception: %s', $message);
