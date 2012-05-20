6106adf95ce3a18ee4fad813e4b4b334b4e3ce66
Matt Robinson <matt@inanimatt.com>
2012-05-20 20:22:26 +0200

    Documentation and validation

--- src/Inanimatt/SiteBuilder/SiteBuilderTemplate.php
+++ src/Inanimatt/SiteBuilder/SiteBuilderTemplate.php
@@ -1,33 +1,97 @@
 <?php
 namespace Inanimatt\SiteBuilder;
 
-/* Credit (but no blame) to Chad Emrys Minick for this Template pattern */
+use Inanimatt\SiteBuilder\Exception\ArgumentException;
+use Inanimatt\SiteBuilder\Exception\RenderException;
+
+/**
+ * Basic PHP-based template class.
+ * 
+ * Credit (but no blame) to Chad Emrys Minick for this Template pattern.
+ * Major modifications:
+ *  - namespaced :P
+ *  - bulk setters (for setup) and getters (for testing)
+ *  - extract() has the extra argument to prevent overwriting of 
+ *    non-template variables. 
+ */
 class SiteBuilderTemplate {
-	private $vars = array(
-	);
+    private $vars = array(
+    );
 
-	public function __get($name) {
-		return $this->vars[$name];
-	}
+    /**
+     * Fetch template var.
+     * 
+     * @param string $name Variable name to fetch
+     * @return mixed Requested variable or null
+     */
+    public function __get($name) {
+        if (!is_string($name)) {
+            throw new ArgumentException($name . ' is not a valid variable name');
+        }
+        
+        if (!isset($this->vars[$name])) {
+            return null;
+        }
+        
+        return $this->vars[$name];
+    }
  
-	public function __set($name, $value) {
-		$this->vars[$name] = $value;
-	}
+    /**
+     * Set template var.
+     * 
+     * @param string $name Variable name to fetch
+     * @param mixed $value Variable value
+     * @return null
+     */
+    public function __set($name, $value) {
+        $this->vars[$name] = $value;
+    }
 
-	public function __getVars() {
-		return $this->vars;
-	}
+    /**
+     * Retrieve all vars
+     * 
+     * @return array Template variables
+     */
+    public function __getVars() {
+        return $this->vars;
+    }
 
-	public function __setVars($vars) {
-		$this->vars = $vars;
-	}
+    /**
+     * Bulk set template variables
+     * 
+     * @param array $vars Template variables (array or array-like)
+     * @return null
+     */
+    public function __setVars($vars) {
+        
+        if (! (is_array($vars) || ($vars instanceof \ArrayAccess)) ) {
+            throw new ArgumentException('Argument 1 must be an array or array-like.');
+        }
+        
+        $this->vars = $vars;
+    }
  
-	public function render($__file) {
-		extract($this->vars, EXTR_SKIP);
-		ob_start();
-		include($__file);
-		return ob_get_clean();
-	}
+ 
+    /**
+     * Render template.
+     * 
+     * Extracts all the template variables into the local scope (without
+     * overwriting existing template vars, like $this, which would be weird)
+     * and then includes the given file, capturing all output in a buffer,
+     * which is returned.
+     * 
+     * @return string Rendered template
+     */
+    public function render($__file) {
+        if (!is_file($__file)) {
+            throw new RenderException('Template file does not exist.');
+        }
+        
+        extract($this->vars, EXTR_SKIP);
+        ob_start();
+        include($__file);
+        return ob_get_clean();
+    }
 }
 
 
