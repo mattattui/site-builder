d30d33262db310b19c70b13467e7dffd2f0f5b88
Matt Robinson <matt@inanimatt.com>
2012-05-20 20:24:52 +0200

    New serialiser exception class

---
+++ src/Inanimatt/SiteBuilder/Exception/SerialiserException.php
@@ -0,0 +1,20 @@
+<?php
+namespace Inanimatt\SiteBuilder\Exception;
+
+/**
+ * Serialiser Exception.
+ * 
+ * Thrown by serialisers, usually when they are unable to write to their
+ * output device.
+ */
+
+class SerialiserException extends SiteBuilderException 
+{
+
+    public function __construct($message = null, $code = 0, \Exception $previous = null)
+    {
+        $message = sprintf('Serialiser exception: %s', $message);
+        return parent::__construct($message, $code, $previous);
+    }
+    
+}
