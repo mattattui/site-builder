debc63c515ad8673139d006779d3d39b79855f20
Matt Robinson <matt@inanimatt.com>
2012-05-20 20:24:34 +0200

    Documentation

--- src/Inanimatt/SiteBuilder/Serialiser/SerialiserInterface.php
+++ src/Inanimatt/SiteBuilder/Serialiser/SerialiserInterface.php
@@ -2,9 +2,24 @@
     
 namespace Inanimatt\SiteBuilder\Serialiser;
 
+/**
+ * Serialiser interface.
+ * 
+ * Serialisers are expected to write rendered content to an output device
+ * like a local disk, a remote file share, or web service. 
+ */
 interface SerialiserInterface
 {
 
+    /**
+     * Write content that corresponds to the given relative URL.
+     * 
+     * @param string $content Rendered HTML content
+     * @param string $path Expected URL
+     * 
+     * @throws Inanimatt\SiteBuilder\Exception\SerialiserException
+     * @return null
+     */
     public function write($content, $path);
     
 }
