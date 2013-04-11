5aaa48fb94d84295b06e0b954ae422c27ca8cbec
Matt Robinson <matt@inanimatt.com>
2013-04-11 01:23:00 +0100

    Restore overwritted tests, fix test executable

---
+++ tests/Inanimatt/SiteBuilder/Transformer/SassProcessBuilderTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Inanimatt\SiteBuilder\Transformer;
+
+use \Mockery as m;
+use Symfony\Component\Process\Process;
+
+class SassProcessBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SassProcessBuilder
+     */
+    protected $object;
+
+    public function testIsInstalledSucceeds()
+    {
+        // Safe to assume shell is installed?
+        $object = new SassProcessBuilder('/bin/sh');
+        $this->assertTrue($object->isInstalled());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidExecutable()
+    {
+        // Safe to assume shell is installed?
+        $object = new SassProcessBuilder('NoSuchFile');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidStyle()
+    {
+        // Safe to assume shell is installed?
+        $object = new SassProcessBuilder('/bin/sh', 'NoSuchStyle');
+    }
+
+    public function testGetProcess()
+    {
+        // Safe to assume shell is installed?
+        $object = new SassProcessBuilder('/bin/sh');
+        $process = $object->getProcess('filename.scss');
+        
+        $this->assertTrue($process instanceof Process);
+        $this->assertTrue(strpos($process->getCommandLine(), '--scss') !== false);
+    }
+
+    public function testGetSassProcess()
+    {
+        // Safe to assume PHP binary is installed…?
+        $object = new SassProcessBuilder($_SERVER['_']);
+        $process = $object->getProcess('filename.sass');
+        
+        $this->assertTrue($process instanceof Process);
+        $this->assertTrue(strpos($process->getCommandLine(), '--scss') === false);
+    }
+
+}
