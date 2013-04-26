0dd0593d9c70565a0ef9b91666ee802dcd5b1fb9
inouire <edouard@melix.net>
2013-04-26 17:15:17 +0200

    Add new transformer dedicated to page context

---
+++ src/Inanimatt/SiteBuilder/Transformer/PagecontextTransformer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Inanimatt\SiteBuilder\Transformer;
+
+use Inanimatt\SiteBuilder\Event\FileCopyEvent;
+use Inanimatt\SiteBuilder\Transformer\TransformerInterface;
+use \Twig_Environment;
+
+class PagecontextTransformer implements TransformerInterface
+{
+    protected $output_path;
+
+    public function __construct($output_path)
+    {
+        if (substr($output_path, -1) !== DIRECTORY_SEPARATOR) {
+            $output_path .= DIRECTORY_SEPARATOR;
+        }
+        $this->output_path = $output_path;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform(FileCopyEvent $event)
+    {
+        $relative_path = str_replace($this->output_path, '', $event->getTarget());
+        $site_root = str_repeat('..'.DIRECTORY_SEPARATOR, substr_count($relative_path, DIRECTORY_SEPARATOR));
+		$path_parts = pathinfo($relative_path);
+		$page_name = $path_parts['filename'];
+		$path = explode(DIRECTORY_SEPARATOR, $relative_path, -1);
+		
+        $event->data->set('siteroot', $site_root);
+        $event->data->set('breadcrumbs', $path);
+        $event->data->set('pagename', $page_name);
+    }
+}
